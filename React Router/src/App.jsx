d8f3a39a5fe9e191f960a22cc4aea489bac2f072
Roy <rjperlman@gmail.com>
2024-12-06 12:14:04 -0800

    Some Updates.

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import Blue from './components/Blue.jsx';
 import Red from './components/Red.jsx';
+import Home from './components/Home.jsx';
 
 function App() 
 {
@@ -14,11 +15,13 @@ function App()
       <div id="navbar">
         <Link to="/blue">blue</Link>
         <Link to="/red">red</Link>
+        <Link to="/">home</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<h1>Blue</h1>} />
           <Route path="/red" element={<h1>Red</h1>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </div>);
---
+++ React Router/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() 
+{
+    return(
+      <div className="home">
+        <h1>HOME</h1>
+      </div>
+    );
+  }
