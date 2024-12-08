884cb512d85fa6ca98fc0475388a3786164c7379
Roy <rjperlman@gmail.com>
2024-12-07 16:29:10 -0800

    Fixed Some Bugs.

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -11,15 +11,6 @@ import Home from './components/Home.jsx';
 
 function App() 
 {
-
-  /*
-  return (<div id="container">
-      <h1>Hello React Router!</h1>)
-        <Navbar />
-        <MainContainer />
-      </div>);
-*/
-  
   return(
     <div id="container">
       <h1>Hello React Router!</h1>
